ecfb2a54cdbd70e6fa0d3c38d2c6ee0ceee76749
Raouf fer <feraouf91@gmail.com>
2025-08-25 17:40:54 +0100

    fixed hero button

--- Crypto-Immobilier-Front/src/App.jsx
+++ Crypto-Immobilier-Front/src/App.jsx
@@ -50,7 +50,7 @@ function App() {
             id="home"
             initial="hidden"
             whileInView="visible"
-            viewport={{ once: false, amount: 0.3 }}
+            viewport={{ once: true, amount: 0.3 }}
             variants={sectionVariants}
           >
             <Hero />
